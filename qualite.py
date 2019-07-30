f2cf68d4237efdb696ef67ce7fc44053b7036c43
achauviere <antoine.chauviere@etu.univ-nantes.fr>
2019-07-30 14:19:52 +0200

    kai

--- qualite.py
+++ qualite.py
@@ -33,7 +33,7 @@ def qualite_model(num, path_model_seg, time, wei=None):
     # Detection label
     tab2 = tab[np.where(tab[:,1]==num)]
     detect_annot = tab2[:,2][np.where(tab2[:,3]==1)]
-    n = len(detect_annot) # -1 pour Souris 56 ( surement erreur dans annotation )
+    n = len(detect_annot) -1 # -1 pour Souris 56 ( surement erreur dans annotation )
 
     # Segmentation label
     list_msk = utils.sorted_aphanumeric(os.listdir(path_souris_annoter))
